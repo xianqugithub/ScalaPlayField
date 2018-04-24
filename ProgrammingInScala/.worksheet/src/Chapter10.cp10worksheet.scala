aaedba66d62414e855e33a69cd1abdb2842d8efa
xianqugithub <chris.quxian@gmail.com>
2018-04-24 16:21:30 -0700

    Chapter10Spiral

---
+++ ProgrammingInScala/.worksheet/src/Chapter10.cp10worksheet.scala
@@ -0,0 +1,15 @@
+package Chapter10
+
+import Element.elem
+import Spiral.spiral
+
+
+object cp10worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(121); val res$0 = 
+    Array.fill(3)("|") mkString "\n";System.out.println("""res0: String = """ + $show(res$0));$skip(25); val res$1 = 
+    Array.fill(1)("-"*3);System.out.println("""res1: Array[String] = """ + $show(res$1))}
+  
+  
+                                    
+                                    
+  
+}
---
+++ ProgrammingInScala/src/Chapter10/cp10worksheet.sc
@@ -0,0 +1,17 @@
+package Chapter10
+
+import Element.elem
+import Spiral.spiral
+
+
+object cp10worksheet {
+    Array.fill(3)("|") mkString "\n"              //> res0: String = |
+                                                  //| |
+                                                  //| |
+    Array.fill(1)("-"*3)                          //> res1: Array[String] = Array(---)
+  
+  
+                                    
+                                    
+  
+}
